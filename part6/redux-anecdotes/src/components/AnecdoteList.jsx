d1d5ccdfe7cf09b39111d0121a0ed48fa6a89fb2
Dvwhite81 <Dvwhite1981@gmail.com>
2023-10-20 10:42:57 -0400

    6.9 - complete

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -15,7 +15,14 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if ( filter === 'ALL' ) {
+      return anecdotes
+    }
+
+    const regex = new RegExp(filter, 'i')
+    return anecdotes.filter(anecdote => anecdote.content.match(regex))
+  })
 
   return (
     <div className='anecdote-container'>
